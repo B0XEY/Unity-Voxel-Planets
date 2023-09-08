5adcf93ebcc58d205aab27fde25c11040627d8b7
B0XEY <gramcooker@gmail.com>
2023-09-08 18:12:11 -0500

    Full fixed code
    Should work with no issue

--- Assets/Boxey/Planets/Core/Generation/Terraformer.cs
+++ Assets/Boxey/Planets/Core/Generation/Terraformer.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 namespace Boxey.Planets.Core.Generation {
     public class Terraformer : MonoBehaviour {
+        private GameObject _point;
         private PlanetCreator _target;
         private PlanetList _list;
         private bool _terraforming;
@@ -17,13 +18,11 @@ namespace Boxey.Planets.Core.Generation {
         [Header("Terraforming")]
         [Line]
         [SerializeField] private bool doTerraform = true;
-        [ShowIf("@doTerraform")]
-        [SerializeField] private Transform point;
-        [ShowIf("@doTerraform")]
+        [ShowIf("doTerraform")]
         [SerializeField, Range(1,5)] private int updateRange = 1;
-        [ShowIf("@doTerraform")]
+        [ShowIf("doTerraform")]
         [SerializeField, Range(.25f, 10)] private float brushRadius = 2.5f;
-        [ShowIf("@doTerraform")]
+        [ShowIf("doTerraform")]
         [SerializeField,Range(0.01f, 1f)] private float brushSpeed = .4f;
 
         [Header("Planets")]
@@ -31,21 +30,22 @@ namespace Boxey.Planets.Core.Generation {
         [SerializeField] private PlanetCreator[] planets;
 
         private void Awake() {
-            TryGetComponent(out _list);
+            _list = FindObjectOfType<PlanetList>();
+            _point = new GameObject("Terraform Point");
         }
         private void Update() {
             if (planets.Length == 0) return;
             var ray = Helpers.GetCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit)) {
-                point.position = hit.point;
+                _point.transform.position = hit.point;
                 if (hit.transform.name == "Sun") return;
                 if (!doTerraform && hit.transform.gameObject.layer != 3) return;
                 _target = hit.transform.parent.GetComponentInParent<PlanetCreator>();
                 if (_target == null) return;
-                point.SetParent(_target.transform);
+                _point.transform.SetParent(_target.transform);
                 var numbers = hit.transform.name.Split(",");
                 var currentChunk = new Vector3Int(int.Parse(numbers[0]), int.Parse(numbers[1]), int.Parse(numbers[2]));
-                float3 terraformPoint = (point.localPosition + -_target.chunkOffset);
+                float3 terraformPoint = (_point.transform.localPosition + -_target.chunkOffset);
                 if (Input.GetKey(KeyCode.Mouse0)) {
                     _terraforming = true;
                     _target.Terrafrom(currentChunk, terraformPoint, updateRange, brushRadius, brushSpeed, true);
